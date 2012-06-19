9cb8f2190570d1341affb9eecf0cffe8be8a1a1f
rickard <rickard@montania.se>
2012-06-19 21:35:41 +0200

    test commit

---
+++ tests/test_utcw_class.php
@@ -0,0 +1,18 @@
+<?php
+
+class UTCW_Test_Class extends WP_UnitTestCase {
+
+	function test_class_exists()
+	{
+		$this->assertTrue( class_exists( 'UTCW' ) );
+	}
+
+	function test_class_inherits_wp_widget()
+	{
+		$this->assertTrue( new UTCW( '', '' ) instanceof WP_Widget );
+	}
+
+	function test_fail() {
+		$this->assertTrue( 1 == 2 );
+	}
+}
---
+++ utcw.php
@@ -0,0 +1,14 @@
+<?php if (!defined("ABSPATH")) die();
+/*
+Plugin Name: Ultimate tag cloud widget
+Plugin URI: http://www.0x539.se/wordpress/ultimate-tag-cloud-widget/
+Description: This plugin aims to be the most configurable tag cloud widget out there, able to suit all your weird tag cloud needs.
+Version: 2.0 alpha
+Author: Rickard Andersson
+Author URI: http://www.0x539.se
+License: GPLv2
+*/
+
+class UTCW extends WP_Widget {
+
+}

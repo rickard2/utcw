f24c55dbbd91df9f09e3b78f60f62ee0ef091341
rickard <rickard@montania.se>
2012-07-09 19:48:35 +0200

    Rewrote all tests to separate test for each option Added docblocks Added missing configuration options to UTCW_Config

--- tests/test_utcw_config.php
+++ tests/test_utcw_config.php
@@ -2,117 +2,601 @@
 
 class UTCW_Test_Config extends WP_UnitTestCase {
 
-	function test_config_defaults() {
+	function test_config_defaults()
+	{
 
-		$reflection = new ReflectionClass('UTCW_Config');
+		$reflection = new ReflectionClass( 'UTCW_Config' );
 		$properties = $reflection->getDefaultProperties();
-		$options   = $properties['options'];
+		$options    = $properties[ 'options' ];
 
-		$config   = new UTCW_Config(array());
+		$config   = new UTCW_Config( array() );
 		$instance = $config->get_instance();
 
 		$this->assertEquals( $options, $instance );
 	}
 
-	function test_config_valid() {
-
-		$my_instance = $config = array(
-			'title'              => 'test',
-			'order'              => 'random',
-			'size_from'          => 1337,
-			'size_to'            => 1337,
-			'max'                => 1337,
-			// 'taxonomy'           => 'post_tag',
-			'reverse'            => true,
-			'color'              => 'random',
-			'letter_spacing'     => 1337,
-			'word_spacing'       => 1337,
-			'case'               => 'capitalize',
-			'case_sensitive'     => true,
-			'minimum'            => 1337,
-			'tags_list_type'     => 'include',
-			'show_title'         => true,
-			'link_underline'     => 'yes',
-			'link_bold'          => 'yes',
-			'link_italic'        => 'yes',
-			'link_bg_color'      => '#123456',
-			'link_border_style'  => 'dotted',
-			'link_border_width'  => 1337,
-			'link_border_color'  => '#123456',
-			'hover_underline'    => 'yes',
-			'hover_bold'         => 'yes',
-			'hover_italic'       => 'yes',
-			'hover_bg_color'     => '#123456',
-			'hover_color'        => '#123456',
-			'hover_border_style' => 'groove',
-			'hover_border_width' => 1337,
-			'hover_border_color' => '#123456',
-			'tag_spacing'        => 1337,
-			'debug'              => true,
-			'days_old'           => 1337,
-			'line_height'        => 1337,
-			'separator'          => 'test',
-			'prefix'             => 'test',
-			'suffix'             => 'test',
-			'show_title_text'    => true,
-		);
-
-		$config = new UTCW_Config( $my_instance );
-		$instance = $config->get_instance();
-
-		foreach ( $my_instance as $key => $item ) {
-			$this->assertEquals( $item, $instance[ $key ] );
-		}
-
+	function test_title_ok()
+	{
+		$this->helper_string_ok( 'title' );
 	}
 
-	function test_config_invalid() {
+	function test_title_fail()
+	{
+		$this->helper_string_fail( 'title' );
+	}
 
-		$my_instance = $config = array(
-			'title'              => false,
-			'order'              => 'order',
-			'size_from'          => 'size_from',
-			'size_to'            => 'size_to',
-			'max'                => 'max',
-			'reverse'            => 'reverse',
-			'color'              => 'color',
-			'letter_spacing'     => 'letter_spacing',
-			'word_spacing'       => 'word_spacing',
-			'case'               => 'case',
-			'case_sensitive'     => 'case_sensitive',
-			'minimum'            => 'minimum',
-			'tags_list_type'     => 'tags_list_type',
-			'show_title'         => 'show_title',
-			'link_underline'     => 'link_underline',
-			'link_bold'          => 'link_bold',
-			'link_italic'        => 'link_italic',
-			'link_bg_color'      => 'link_bg_color',
-			'link_border_style'  => 'link_border_style',
-			'link_border_width'  => 'link_border_width',
-			'link_border_color'  => 'link_border_color',
-			'hover_underline'    => 'hover_underline',
-			'hover_bold'         => 'hover_bold',
-			'hover_italic'       => 'hover_italic',
-			'hover_bg_color'     => 'hover_bg_color',
-			'hover_color'        => 'hover_color',
-			'hover_border_style' => 'hover_border_style',
-			'hover_border_width' => 'hover_border_width',
-			'hover_border_color' => 'hover_border_color',
-			'tag_spacing'        => 'tag_spacing',
-			'debug'              => 'debug',
-			'days_old'           => 'days_old',
-			'line_height'        => 'line_height',
-			'separator'          => false,
-			//'prefix'             => false,
-			//'suffix'             => false,
-			'show_title_text'    => 'show_title_text',
-		);
+	function test_order_ok()
+	{
+		$this->helper_string_ok( 'order', 'count' );
+	}
 
-		$config = new UTCW_Config( $my_instance );
-		$instance = $config->get_instance();
+	function test_order_fail()
+	{
+		$this->helper_string_fail( 'order', 'invalid order' );
+	}
 
-		foreach ( $my_instance as $key => $item ) {
-			$this->assertNotEquals( $item, $instance[ $key ] );
-		}
+	function test_size_from_ok()
+	{
+		$this->helper_int_ok( 'size_from' );
+	}
+
+	function test_size_from_fail()
+	{
+		$this->helper_int_fail( 'size_from' );
+	}
+
+	function test_size_from_zero_fail()
+	{
+		$this->helper_int_fail( 'size_from', 0 );
+	}
+
+	function test_size_to_ok()
+	{
+		$this->helper_int_ok( 'size_to' );
+	}
+
+	function test_size_to_fail()
+	{
+		$this->helper_int_fail( 'size_to' );
+	}
+
+	function test_size_to_zero_fail()
+	{
+		$this->helper_int_fail( 'size_to', 0 );
+	}
+
+	function test_max_ok()
+	{
+		$this->helper_int_ok( 'max' );
+	}
+
+	function test_max_fail()
+	{
+		$this->helper_int_fail( 'max' );
+	}
+
+	function test_max_zero_fail()
+	{
+		$this->helper_int_fail( 'max', 0 );
+	}
+
+	function test_reverse_ok()
+	{
+		$this->helper_bool_ok( 'reverse' );
+	}
+
+	function test_reverse_fail()
+	{
+		$this->helper_bool_fail( 'reverse' );
+	}
+
+	function test_taxonomy_ok()
+	{
+		$this->helper_string_ok( 'taxonomy', 'post_tag' );
+	}
+
+	function test_taxonomy_fail()
+	{
+		$this->helper_string_fail( 'taxonomy', 'invalid taxonomy' );
+	}
+
+	function test_color_ok()
+	{
+		$this->helper_string_ok( 'color', 'random' );
+	}
+
+	function test_color_fail()
+	{
+		$this->helper_string_fail( 'color', 'invalid color' );
+	}
+
+	function test_letter_spacing_ok()
+	{
+		$this->helper_int_ok( 'letter_spacing' );
+	}
+
+	function test_letter_spacing_fail()
+	{
+		$this->helper_int_fail( 'letter_spacing' );
+	}
+
+	function test_word_spacing_ok()
+	{
+		$this->helper_int_ok( 'word_spacing' );
+	}
+
+	function test_word_spacing_fail()
+	{
+		$this->helper_int_fail( 'word_spacing' );
+	}
+
+	function test_case_ok()
+	{
+		$this->helper_string_ok( 'case', 'capitalize' );
+	}
+
+	function test_case_fail()
+	{
+		$this->helper_string_fail( 'case', 'invalid case' );
+	}
+
+	function test_case_sensitive_ok()
+	{
+		$this->helper_bool_ok( 'case_sensitive' );
+	}
+
+	function test_case_sensitive_fail()
+	{
+		$this->helper_bool_fail( 'case_sensitive' );
+	}
+
+	function test_minimum_ok()
+	{
+		$this->helper_int_ok( 'minimum' );
+	}
+
+	function test_minimum_fail()
+	{
+		$this->helper_int_fail( 'minimum' );
+	}
+
+	function test_minimum_zero_fail()
+	{
+		$this->helper_int_fail( 'minimum', 0 );
+	}
+
+	function test_tags_list_type_ok()
+	{
+		$this->helper_string_ok( 'tags_list_type', 'include' );
+	}
+
+	function test_tags_list_type_fail()
+	{
+		$this->helper_string_fail( 'tags_list_type', 'invalid type' );
+	}
+
+	function test_show_title_ok()
+	{
+		$this->helper_bool_ok( 'show_title' );
+	}
+
+	function test_show_title_fail()
+	{
+		$this->helper_bool_fail( 'show_title' );
+	}
+
+	function test_link_underline_ok()
+	{
+		$this->helper_optional_bool_ok( 'link_underline' );
+	}
+
+	function test_link_underline_fail()
+	{
+		$this->helper_optional_bool_fail( 'link_underline' );
+	}
+
+	function test_link_bold_ok()
+	{
+		$this->helper_optional_bool_ok( 'link_bold' );
+	}
+
+	function test_link_bold_fail()
+	{
+		$this->helper_optional_bool_fail( 'link_bold' );
+	}
+
+	function test_link_italic_ok()
+	{
+		$this->helper_optional_bool_ok( 'link_italic' );
+	}
+
+	function test_link_italic_fail()
+	{
+		$this->helper_optional_bool_fail( 'link_italic' );
+	}
+
+	function test_link_bg_color_ok()
+	{
+		$this->helper_color_ok( 'link_bg_color' );
+	}
+
+	function test_link_bg_color_fail()
+	{
+		$this->helper_color_fail( 'link_bg_color' );
+	}
+
+	function test_link_border_style_ok()
+	{
+		$this->helper_string_ok( 'link_border_style', 'dashed' );
+	}
+
+	function test_link_border_style_fail()
+	{
+		$this->helper_string_fail( 'link_border_style', 'invalid border style' );
+	}
+
+	function test_link_border_width_ok()
+	{
+		$this->helper_int_ok( 'link_border_width' );
+	}
+
+	function test_link_border_width_fail()
+	{
+		$this->helper_int_fail( 'link_border_width' );
+	}
+
+	function test_link_border_width_zero_ok()
+	{
+		$this->helper_int_ok( 'link_border_width', 0 );
+	}
+
+	function test_link_border_color_ok()
+	{
+		$this->helper_color_ok( 'link_border_color' );
+	}
+
+	function test_link_border_color_fail()
+	{
+		$this->helper_color_fail( 'link_border_color' );
+	}
+
+	function test_hover_underline_ok()
+	{
+		$this->helper_optional_bool_ok( 'hover_underline' );
+	}
+
+	function test_hover_underline_fail()
+	{
+		$this->helper_optional_bool_fail( 'hover_underline' );
+	}
+
+	function test_hover_bold_ok()
+	{
+		$this->helper_optional_bool_ok( 'hover_bold' );
+	}
+
+	function test_hover_bold_fail()
+	{
+		$this->helper_optional_bool_fail( 'hover_bold' );
+	}
+
+	function test_hover_italic_ok()
+	{
+		$this->helper_optional_bool_ok( 'hover_italic' );
+	}
+
+	function test_hover_italic_fail()
+	{
+		$this->helper_optional_bool_fail( 'hover_italic' );
+	}
+
+	function test_hover_bg_color_ok()
+	{
+		$this->helper_color_ok( 'hover_bg_color' );
+	}
+
+	function test_hover_bg_color_fail()
+	{
+		$this->helper_color_fail( 'hover_bg_color' );
+	}
+
+	function test_hover_color_ok()
+	{
+		$this->helper_color_ok( 'hover_color' );
+	}
+
+	function test_hover_color_fail()
+	{
+		$this->helper_color_fail( 'hover_color' );
+	}
+
+	function test_hover_border_style_ok()
+	{
+		$this->helper_string_ok( 'hover_border_style', 'groove' );
+	}
+
+	function test_hover_border_style_fail()
+	{
+		$this->helper_string_fail( 'hover_border_style', 'invalid border style' );
+	}
+
+	function test_hover_border_width_ok()
+	{
+		$this->helper_int_ok( 'hover_border_width' );
+	}
+
+	function test_hover_border_width_fail()
+	{
+		$this->helper_int_fail( 'hover_border_width' );
+	}
+
+	function test_hover_border_width_zero_ok()
+	{
+		$this->helper_int_ok( 'hover_border_width', 0 );
+	}
+
+	function test_hover_border_color_ok()
+	{
+		$this->helper_color_ok( 'hover_border_color' );
+	}
+
+	function test_hover_border_color_fail()
+	{
+		$this->helper_color_fail( 'hover_border_color' );
+	}
+
+	function test_tag_spacing_ok()
+	{
+		$this->helper_int_ok( 'tag_spacing' );
+	}
+
+	function test_tag_spacing_fail()
+	{
+		$this->helper_int_fail( 'tag_spacing' );
+	}
+
+	function test_debug_ok()
+	{
+		$this->helper_bool_ok( 'debug' );
+	}
+
+	function test_debug_fail()
+	{
+		$this->helper_bool_fail( 'debug' );
+	}
+
+	function test_days_old_ok()
+	{
+		$this->helper_int_ok( 'days_old' );
+	}
+
+	function test_days_old_fail()
+	{
+		$this->helper_int_fail( 'days_old' );
+	}
+
+	function test_days_old_zero_ok()
+	{
+		$this->helper_int_ok( 'days_old', 0 );
+	}
+
+	function test_line_height_ok()
+	{
+		$this->helper_int_ok( 'line_height' );
+	}
+
+	function test_line_height_fail()
+	{
+		$this->helper_int_fail( 'line_height' );
+	}
+
+	function test_separator_ok()
+	{
+		$this->helper_string_ok( 'separator' );
+	}
+
+	function test_prefix_ok()
+	{
+		$this->helper_string_ok( 'prefix' );
+	}
+
+	function test_suffix_ok()
+	{
+		$this->helper_string_ok( 'suffix' );
+	}
+
+	function test_prefix_empty_ok()
+	{
+		$this->helper_string_ok( 'prefix', '' );
+	}
+
+	function test_suffix_empty_ok()
+	{
+		$this->helper_string_ok( 'suffix', '' );
+	}
+
+	function test_show_title_text_ok()
+	{
+		$this->helper_bool_ok( 'show_title_text' );
+	}
+
+	function test_show_title_text_fail()
+	{
+		$this->helper_bool_fail( 'show_title_text' );
+	}
+
+	function test_authors_ok()
+	{
+		$this->helper_int_array_ok( 'authors' );
+	}
+
+	function test_authors_fail()
+	{
+		$this->helper_int_array_fail( 'authors' );
+	}
+
+	function test_authors_csv_ok()
+	{
+		$this->helper_array_ok( 'authors', '1,2,3' );
+	}
+
+	function test_authors_empty_ok()
+	{
+		$this->helper_int_array_ok( 'authors', array() );
+	}
+
+	function test_post_type_ok()
+	{
+		$this->helper_array_ok( 'post_type', array( 'post' ) );
+	}
+
+	function test_post_type_fail()
+	{
+		$this->helper_array_fail( 'post_type', array( 'invalid post type' ) );
+	}
+
+	function test_post_type_csv_ok()
+	{
+		// TODO: this test should include multiple values but without dynamic fetching of post types there's only one valid value
+		$this->helper_array_ok( 'post_type', 'post' );
+	}
+
+	function test_color_span_from_ok()
+	{
+		$this->helper_color_ok( 'color_span_from' );
+	}
+
+	function test_color_span_from_fail()
+	{
+		$this->helper_color_fail( 'color_span_to' );
+	}
+
+	function test_color_span_to_ok()
+	{
+		$this->helper_color_ok( 'color_span_to' );
+	}
+
+	function test_color_span_to_fail()
+	{
+		$this->helper_color_fail( 'color_span_to' );
+	}
+
+	function test_tags_list_ok()
+	{
+		$this->helper_int_array_ok( 'tags_list' );
+	}
+
+	function test_tags_list_fail()
+	{
+		$this->helper_int_array_fail( 'tags_list' );
+	}
+
+	function test_unknown_attribute()
+	{
+		$attr              = '__unknown';
+		$instance[ $attr ] = 'value';
+		$config            = new UTCW_Config( $instance );
+		$this->assertFalse( isset( $config->$attr ) );
+	}
+
+	private function helper_string_ok( $option, $ok_string = 'test' )
+	{
+		$instance[ $option ] = $ok_string;
+		$config              = new UTCW_Config( $instance );
+		$this->assertEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_string_fail( $option, $fail_string = '' )
+	{
+		$instance[ $option ] = $fail_string;
+		$config              = new UTCW_Config( $instance );
+		$this->assertNotEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_int_ok( $option, $ok_int = 10 )
+	{
+		$instance[ $option ] = $ok_int;
+		$config              = new UTCW_Config( $instance );
+		$this->assertEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_int_fail( $option, $fail_int = 'fail' )
+	{
+		$instance[ $option ] = $fail_int;
+		$config              = new UTCW_Config( $instance );
+		$this->assertNotEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_bool_ok( $option, $ok_bool = true )
+	{
+		$instance[ $option ] = $ok_bool;
+		$config              = new UTCW_Config( $instance );
+		$this->assertEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_bool_fail( $option, $fail_bool = 'fail' )
+	{
+		$instance[ $option ] = $fail_bool;
+		$config              = new UTCW_Config( $instance );
+		$this->assertNotEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_optional_bool_ok( $option, $ok_opt_bool = 'yes' )
+	{
+		$instance[ $option ] = $ok_opt_bool;
+		$config              = new UTCW_Config( $instance );
+		$this->assertEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_optional_bool_fail( $option, $fail_opt_bool = 'fail' )
+	{
+		$instance[ $option ] = $fail_opt_bool;
+		$config              = new UTCW_Config( $instance );
+		$this->assertNotEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_int_array_ok( $option, $ok_int_array = array( 1, 2, 3 ) )
+	{
+		$instance[ $option ] = $ok_int_array;
+		$config              = new UTCW_Config( $instance );
+		$this->assertEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_int_array_fail( $option, $fail_int_array = array( 'fail', 'more fail' ) )
+	{
+		$instance[ $option ] = $fail_int_array;
+		$config              = new UTCW_Config( $instance );
+		$this->assertNotEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_color_ok( $option, $ok_color = '#bada55' )
+	{
+		$instance[ $option ] = $ok_color;
+		$config              = new UTCW_Config( $instance );
+		$this->assertEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_color_fail( $option, $fail_color = 'invalid color' )
+	{
+		$instance[ $option ] = $fail_color;
+		$config              = new UTCW_Config( $instance );
+		$this->assertNotEquals( $config->$option, $instance[ $option ] );
+	}
+
+	private function helper_array_ok( $option, $ok_array = array( 'test' ) )
+	{
+		$instance[ $option ] = $ok_array;
+		$config              = new UTCW_Config( $instance );
+
+		$result = is_array( $ok_array ) ? $ok_array : explode( ',', $ok_array );
+
+		$this->assertEquals( $config->$option, $result );
+	}
+
+	private function helper_array_fail( $option, $fail_array = 'not an array' )
+	{
+		$instance[ $option ] = $fail_array;
+		$config              = new UTCW_Config( $instance );
+		$this->assertNotEquals( $config->$option, $instance[ $option ] );
 	}
 }
--- utcw-config.php
+++ utcw-config.php
@@ -2,60 +2,272 @@
 
 /**
  * Configuration class for the widget.
+ * @package utcw
  */
 class UTCW_Config {
 
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $title;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $order;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $size_from;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $size_to;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $max;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $taxonomy;
+
+	/**
+	 * @var bool
+	 * @since 2.0
+	 */
 	public $reverse;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $color;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $letter_spacing;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $word_spacing;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $case;
+
+	/**
+	 * @var bool
+	 * @since 2.0
+	 */
 	public $case_sensitive;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $minimum;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $tags_list_type;
+
+	/**
+	 * @var bool
+	 * @since 2.0
+	 */
 	public $show_title;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $link_underline;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $link_bold;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $link_italic;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $link_bg_color;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $link_border_style;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $link_border_width;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $link_border_color;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $hover_underline;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $hover_bold;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $hover_italic;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $hover_bg_color;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $hover_color;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $hover_border_style;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $hover_border_width;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $hover_border_color;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $tag_spacing;
+
+	/**
+	 * @var bool
+	 * @since 2.0
+	 */
 	public $debug;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $days_old;
+
+	/**
+	 * @var int
+	 * @since 2.0
+	 */
 	public $line_height;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $separator;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $prefix;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
 	public $suffix;
+
+	/**
+	 * @var bool
+	 * @since 2.0
+	 */
 	public $show_title_text;
 
-	/*
-	 * Missing:
-	 * authors
-	 * post_type
-	 * color_span_from
-	 * color_span_to
-	 * tags_list
+	/**
+	 * @var array
+	 * @since 2.0
 	 */
+	public $post_type;
+
+	/**
+	 * @var array
+	 * @since 2.0
+	 */
+	public $tags_list;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
+	public $color_span_to;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
+	public $color_span_from;
+
+	/**
+	 * @var string
+	 * @since 2.0
+	 */
+	public $authors;
 
 	/**
 	 * Config store with default values
 	 * @var array
+	 * @since 2.0
 	 */
 	protected $options = array(
 		'title'              => 'Tag Cloud',
@@ -96,40 +308,58 @@ class UTCW_Config {
 		'prefix'             => '',
 		'suffix'             => '',
 		'show_title_text'    => true,
+		'post_type'          => array( 'post' ),
+		'tags_list'          => array(),
+		'color_span_to'      => '',
+		'color_span_from'    => '',
+		'authors'            => array(),
 	);
 
 	/**
 	 * @var array
+	 * @since 2.0
 	 */
 	protected $allowed_orders = array( 'random', 'name', 'slug', 'id', 'color', 'count' );
 
 	/**
 	 * @var array
+	 * @since 2.0
 	 */
 	protected $allowed_taxonomies = array(); // Will be set dynamically at load
 
 	/**
 	 * @var array
+	 * @since 2.0
+	 */
+	protected $allowed_post_types = array(); // Will be set dynamically at load
+
+	/**
+	 * @var array
+	 * @since 2.0
 	 */
 	protected $allowed_colors = array( 'none', 'random', 'set', 'span' );
 
 	/**
 	 * @var array
+	 * @since 2.0
 	 */
 	protected $allowed_cases = array( 'lowercase', 'uppercase', 'capitalize', 'off' );
 
 	/**
 	 * @var array
+	 * @since 2.0
 	 */
 	protected $allowed_tags_list_types = array( 'exclude', 'include' );
 
 	/**
 	 * @var array
+	 * @since 2.0
 	 */
-	protected $allowed_booleans = array( 'yes', 'no', 'default' );
+	protected $allowed_optional_booleans = array( 'yes', 'no', 'default' );
 
 	/**
 	 * @var array
+	 * @since 2.0
 	 */
 	protected $allowed_border_styles = array(
 		'none', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset',
@@ -139,6 +369,8 @@ class UTCW_Config {
 	 * Loads a configuration and parses the options
 	 *
 	 * @param array $input
+	 *
+	 * @since 2.0
 	 */
 	public function __construct( array $input )
 	{
@@ -162,13 +394,14 @@ class UTCW_Config {
 						$valid = in_array( $input[ $key ], $this->allowed_tags_list_types );
 						break;
 
+					// Optional booleans, can be yes, no or default
 					case 'link_underline':
 					case 'link_bold':
 					case 'link_italic':
 					case 'hover_underline':
 					case 'hover_bold':
 					case 'hover_italic':
-						$valid = in_array( $input[ $key ], $this->allowed_booleans );
+						$valid = in_array( $input[ $key ], $this->allowed_optional_booleans );
 						break;
 
 					case 'link_border_style':
@@ -180,12 +413,36 @@ class UTCW_Config {
 						$valid = in_array( $input[ $key ], $this->allowed_colors );
 						break;
 
+					// TODO: Allow more flexibility in color setting, like rgb/rgba/hsl/hsla and named colors
 					case 'link_bg_color':
 					case 'link_border_color':
 					case 'hover_bg_color':
 					case 'hover_color':
 					case 'hover_border_color':
+					case 'color_span_to':
+					case 'color_span_from':
 						$valid = preg_match( '/#([a-f0-9]{6}|[a-f0-9]{3})/i', $input[ $key ] ) > 0;
+						break;
+
+					case 'taxonomy':
+						$valid = in_array( $input[ $key ], $this->allowed_taxonomies );
+						break;
+
+					case 'post_type':
+						if ( ! is_array( $input[ $key ] ) ) {
+							$input[ $key ] = explode( ',', $input[ $key ] );
+						}
+
+						$valid = count( array_intersect( $this->allowed_post_types, $input[ $key ] ) ) == count( $input[ $key ] );
+						break;
+
+					case 'authors':
+					case 'tags_list':
+						if ( ! is_array( $input[ $key ] ) ) {
+							$input[ $key ] = explode( ',', $input[ $key ] );
+						}
+
+						$valid = $this->is_array_numeric( $input[ $key ] );
 						break;
 				}
 
@@ -196,26 +453,45 @@ class UTCW_Config {
 				// Special handling of some properties which have string defaults but integer values expected
 				if ( in_array( $key, array( 'letter_spacing', 'word_spacing', 'tag_spacing', 'line_height' ) ) ) {
 					$this->$key = intval( $input[ $key ] );
-				}
-
-				else if ( is_string( $this->options[ $key ] ) && is_string( $input[ $key ] ) && strlen( $input[ $key ] ) > 0 ) {
+				} else if ( is_string( $this->options[ $key ] ) && is_string( $input[ $key ] ) && strlen( $input[ $key ] ) > 0 ) {
 					$this->$key = $input[ $key ];
-				}
-
-				else if ( is_integer( $this->options[ $key ] ) && $input[ $key ] >= 0 ) {
+				} else if ( is_integer( $this->options[ $key ] ) && $input[ $key ] > 0 ) {
 					$this->$key = intval( $input[ $key ] );
-				}
-
-				else if ( is_bool( $this->options[ $key ] ) ) {
-					$this->$key = !! $input[ $key ];
+				} else if ( is_bool( $this->options[ $key ] ) ) {
+					$this->$key = ! ! $input[ $key ];
+				} else if ( is_array( $this->options[ $key ] ) ) {
+					$this->$key = is_array( $input[ $key ] ) ? $input[ $key ] : explode( ',', $input[ $key ] );
 				}
 			}
 		}
 	}
 
 	/**
+	 * Checks if every item in the array is numeric
+	 *
+	 * @param array $array
+	 *
+	 * @return bool
+	 *
+	 * @since 2.0
+	 */
+	private function is_array_numeric( array $array )
+	{
+		foreach ( $array as $item ) {
+			if ( ! is_numeric( $item ) ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
 	 * Returns the WP_Widget instance
+	 *
 	 * @return array
+	 *
+	 * @since  2.0
 	 */
 	public function get_instance()
 	{

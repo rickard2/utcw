fecb9934bd5e57253094f0262c3e2cba40c86994
rickard <rickard@montania.se>
2012-10-22 23:02:32 +0200

    Added file name from version 1.x to enable upgrading from 1.x and keeping the configuration

---
+++ ultimate-tag-cloud-widget.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Ultimate tag cloud widget
+Plugin URI: https://www.0x539.se/wordpress/ultimate-tag-cloud-widget/
+Description: This plugin aims to be the most configurable tag cloud widget out there, able to suit all your weird tag cloud needs.
+Version: 2.0 beta1
+Author: Rickard Andersson
+Author URI: https://www.0x539.se
+License: GPLv2
+*/
+
+// Legacy file for compatibility with verison 1.x
+require 'utcw.php';
--- utcw.php
+++ utcw.php
@@ -1,13 +1,4 @@
 <?php
-/*
-Plugin Name: Ultimate tag cloud widget
-Plugin URI: https://www.0x539.se/wordpress/ultimate-tag-cloud-widget/
-Description: This plugin aims to be the most configurable tag cloud widget out there, able to suit all your weird tag cloud needs.
-Version: 2.0 beta1
-Author: Rickard Andersson
-Author URI: https://www.0x539.se
-License: GPLv2
-*/
 /**
  * Ultimate Tag Cloud Widget
  * @author     Rickard Andersson <rickard@0x539.se>

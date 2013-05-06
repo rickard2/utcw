78afae870c78e197fbc88fa9714cd74a3665ebba
rickard <rickard@montania.se>
2013-05-06 13:13:15 +0200

    Tests that hooks are working

---
+++ tests/test_utcw_hooks.php
@@ -0,0 +1,27 @@
+<?php
+////use Rickard\UTCW\Widget;
+
+if (!defined('ABSPATH')) {
+    die();
+}
+/**
+ * Ultimate Tag Cloud Widget
+ *
+ * @author     Rickard Andersson <rickard@0x539.se>
+ * @version    2.2.2
+ * @license    GPLv2
+ * @package    utcw
+ * @subpackage test
+ */
+
+class UTCW_Test_Hooks extends WP_UnitTestCase
+{
+    public function test_hooks()
+    {
+        Plugin::getInstance();
+
+        do_action('admin_head-widgets.php');
+        do_action('wp_loaded');
+        do_action('widgets_init');
+    }
+}

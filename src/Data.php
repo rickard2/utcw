14898e7563db28ccf07a91e4acaacbe8e6e1f8e9
rickard <rickard@montania.se>
2013-09-30 21:02:07 +0200

    Version bump for 2.5

--- src/Data.php
+++ src/Data.php
@@ -3,7 +3,7 @@
  * Ultimate Tag Cloud Widget
  *
  * @author     Rickard Andersson <rickard@0x539.se>
- * @version    2.4
+ * @version    2.5
  * @license    GPLv2
  * @package    utcw
  * @subpackage main

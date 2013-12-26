672c4797d40234813d9a778b8906c3b2dbfde87d
rickard <rickard@montania.se>
2013-12-26 10:52:51 +0100

    Version bump for 2.6-beta1 [ci skip]

--- dist/ultimate-tag-cloud-widget.php
+++ dist/ultimate-tag-cloud-widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Ultimate Tag Cloud Widget
 Plugin URI: https://www.0x539.se/wordpress/ultimate-tag-cloud-widget/
 Description: This plugin aims to be the most configurable tag cloud widget out there.
-Version: 2.5
+Version: 2.6-beta1
 Author: Rickard Andersson
 Author URI: https://www.0x539.se
 License: GPLv2
@@ -15,7 +15,7 @@ License: GPLv2
  * @var string
  * @since 2.0
  */
-define('UTCW_VERSION', '2.6-dev');
+define('UTCW_VERSION', '2.6-beta1');
 
 /**
  * If development mode is currently enabled
